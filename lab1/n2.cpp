a41dc41c752a44719c7711d755b47336e797f4ab
Nsf03 <nazarova.sf@phystech.edu>
2022-02-02 15:17:45 +0300

    hw

--- lab1/n2.cpp
+++ lab1/n2.cpp
@@ -5,14 +5,11 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    if (n % 5 == 0) {
-        for (unsigned int i = 0; i < n; i++) {
+    for (unsigned int i = 0; i < n; i++) {
+        if (n % 5 == 0) {
             cout << "@";
-        }
-    } else {
-        for (unsigned int i = 0; i < n; i++) {
+        } else
             cout << "%";
-        }
     }
     return 0;
 }
